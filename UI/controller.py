49fb386d7fd3e299c25fa43b6146f926cff34879
lorenzorivet <lori.grivet@gmail.com>
2024-04-23 15:59:26 +0200

    es 1

--- UI/controller.py
+++ UI/controller.py
@@ -14,11 +14,36 @@ class Controller:
         self._mese = 0
 
     def handle_umidita_media(self, e):
-        pass
+
+        mese=self._view.dd_mese.value
+
+        mese=int(mese)
+
+        if mese<1 or mese > 12:
+            self._view.create_alert("Mese non valido")
+
+        risultato=self._model.calcola_um_media(mese)
+
+
+        self._view.lst_result.clean()
+
+        for a in risultato.keys():
+            self._view.lst_result.controls.append(ft.Text(f"{a}: {risultato[a][0]/risultato[a][1]}"))
+
+
+        self._view.update_page()
 
 
 
     def handle_sequenza(self, e):
+        mese = self._view.dd_mese.value
+
+        mese = int(mese)
+
+        if mese < 1 or mese > 12:
+            self._view.create_alert("Mese non valido")
+
+        b=self._model.calcola_percorso(mese)
         pass
 
     def read_mese(self, e):
--- model/model.py
+++ model/model.py
@@ -1,3 +1,62 @@
+import copy
+
+from database import meteo_dao
+from datetime import datetime
+
 class Model:
     def __init__(self):
+        self.percorsi=[]
         pass
+
+
+    def calcola_um_media(self,mese):
+        risultato={}
+
+        situazioni = meteo_dao.MeteoDao().get_all_situazioni()
+
+        for i in situazioni:
+
+            if i.data.month == mese:
+
+                if risultato.__contains__(i.localita):
+                    a=risultato[i.localita][0]
+                    a += i.umidita
+                    risultato[i.localita][0]=a
+
+                    b = risultato[i.localita][1]
+                    b += 1
+                    risultato[i.localita][1] = b
+
+                else:
+                    risultato[i.localita]=[i.umidita,1]
+
+        return risultato
+
+    def calcola_percorso(self,mese):
+        situazioni = meteo_dao.MeteoDao().get_all_situazioni()
+
+        for i in situazioni:
+            if i.data.day>15 or i.data.month!=mese:
+                situazioni.remove(i)
+        self.ricorsione([],situazioni)
+        a=0
+        return a
+
+
+    def ricorsione(self,percorso,situazioni_rimanenti):
+        if len(situazioni_rimanenti)==0 or len(percorso)==15:
+            self.percorsi.append(copy.deepcopy(percorso))
+
+
+
+        for i in range(len(situazioni_rimanenti)):
+            percorso.append(situazioni_rimanenti[i])
+            nuove_situazioni_rimanenti = situazioni_rimanenti[:i]+situazioni_rimanenti[i+1:]
+            self.ricorsione(percorso,nuove_situazioni_rimanenti)
+            percorso.pop()
+
+
+
+
+
+
